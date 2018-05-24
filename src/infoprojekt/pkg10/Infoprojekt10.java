a67f25d8ce394db4db3968df9adf0b448b991a81
pc_x <pc_x@home>
2018-05-24 13:21:53 +0200

    Stapel test

--- src/infoprojekt/pkg10/Infoprojekt10.java
+++ src/infoprojekt/pkg10/Infoprojekt10.java
@@ -15,12 +15,33 @@ public class Infoprojekt10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int w = 3;
-        int n = 10;
-        int[] stimmen = new int [n];
+        int w = 3; //Kandidaten für die Wahl
+        int n = 10; //Abgegebene Stimmen
+        int[] stimmen = new int [n]; //wer wen gewählt hat
         
         for(int i=0; i<n; i++){
             stimmen[i] = (int) (Math.random()*w+1);
+        }
+        int[] stapel = new int[n];//nach der Methode
+        int e=0; //Zähler wie viel auf dem Stapel ist
+        
+        
+        for(int i=0;i<n;i++){ //so viele durchläufe wie stimmen
+            if(e==0){
+                stapel[e] = stimmen[i];//wenn auf dem Stapel noch nichts liegt
+                e++;
+            }
+            else{
+                if(stimmen[i]==stapel[e-1]){ //wenn auf dem Stapel das gleiche liegt
+                    stapel[e] = stimmen[i];
+                    e++;
+                }
+                else{
+                    stapel[e-1] = 0;//wenn es nicht gleich ist
+                    e = e-1;
+                }
+            }
+            
         }
     }
     
