35b7b97ae31b338e03a78976f203d7338f377b37
l.casares <l.casares@IR1-PC-09.musterschule.schule.paedml>
2018-06-04 09:12:32 +0200

    neu

---
+++ JavaApplication1/src/Infoprojekt/Start.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Infoprojekt;
+
+/**
+ *
+ * @author l.casares
+ */
+public class Start {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      /*  int w = 0; //Kandidaten für die Wahl
+        int n = 0; //Abgegebene Stimmen
+        int[] stimmen = new int [n]; //wer wen gewählt hat
+        
+        for(int i=0; i<n; i++){
+            stimmen[i] = (int) (Math.random()*w+1);
+        }
+        int[] stapel = new int[n];//nach der Methode
+        int e=0; //Zähler wie viel auf dem Stapel ist
+        
+        
+        for(int i=0;i<n;i++){ //so viele durchläufe wie stimmen
+            if(e==0){
+                stapel[e] = stimmen[i];//wenn auf dem Stapel noch nichts liegt
+                e++;
+            }
+            else{
+                if(stimmen[i]==stapel[e-1]){ //wenn auf dem Stapel das gleiche liegt
+                    stapel[e] = stimmen[i];
+                    e++;
+                }
+                else{
+                    stapel[e-1] = 0;//wenn es nicht gleich ist
+                    e = e-1;
+                }
+            }
+            
+        }*/
+          NewJFrame f=new NewJFrame();
+          f.setVisible(true);
+        //System.out.println(stapel[e-1]);
+        
+      
+        
+}
+    
+}
